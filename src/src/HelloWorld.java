f2f6ac128bf67f6bf629b2bbca20ce6b28aee8fa
周林华 <zhoulinhua@gwi.com.cn>
2017-06-14 10:42:35 +0800

    第5次更新。

--- src/src/HelloWorld.java
+++ src/src/HelloWorld.java
@@ -51,6 +51,7 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("git hub!!!!test");
 		System.out.println("aaaaa");
+		System.out.println("²âÊÔgitµÄ");
 	}
 
 	
