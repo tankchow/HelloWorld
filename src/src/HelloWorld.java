b0351d208f6d4a0a71572ac4ccd353979842e922
周林华 <zhoulinhua@gwi.com.cn>
2017-06-14 14:26:56 +0800

    测试Amend
    删除了注释，增加了感叹号

--- src/src/HelloWorld.java
+++ src/src/HelloWorld.java
@@ -10,49 +10,50 @@ package src;
 public class HelloWorld {
 	public static void main(String[] args) {
 
+		// //枚举测试
+		// System.out.println(MyColorsEnum.RED.value());
 
-//		//枚举测试
-//		System.out.println(MyColorsEnum.RED.value());
-		
-//		// lambda表达式测试
-//		 LambdaTest.normalFunc1();
-//		 LambdaTest.lambdaFunc1();
+		// // lambda表达式测试
+		// LambdaTest.normalFunc1();
+		// LambdaTest.lambdaFunc1();
 
-//		// 匿名类用法
-//		ClassTest anonymousClassTest = new ClassTest();
-//		int sum = anonymousClassTest.add(1, 2);
-//		anonymousClassTest = new ClassTest() {
-//			int add(int x, int y) {
-//				String sum = Integer.toString(x) + Integer.toString(y);
-//				return Integer.parseInt(sum);
-//			}
-//		};
-//		int sum1 = anonymousClassTest.add(1, 2);
-//		int dif = anonymousClassTest.sub(5, 2, 1);
+		// // 匿名类用法
+		// ClassTest anonymousClassTest = new ClassTest();
+		// int sum = anonymousClassTest.add(1, 2);
+		// anonymousClassTest = new ClassTest() {
+		// int add(int x, int y) {
+		// String sum = Integer.toString(x) + Integer.toString(y);
+		// return Integer.parseInt(sum);
+		// }
+		// };
+		// int sum1 = anonymousClassTest.add(1, 2);
+		// int dif = anonymousClassTest.sub(5, 2, 1);
 
-//		// 匿名类改成lambda表达式
-//		int sum2 = lambdaFun((int x, int y) -> {
-//			return x + y;
-//		}, 4, 5);
-//		Adder adder = (x, y) -> x + y + x + y;
-//		int sum3 = adder.add(4, 5);
-//		System.out.println(sum + "|" + sum1 + "|" + dif + "|" + sum2 + "|" + sum3);
-		
-		//lambda表达式，方法引用代替lambda表达式
-		//Person[] people = { new Person("zhangsan", 22), new Person("lisi", 39) };
+		// // 匿名类改成lambda表达式
+		// int sum2 = lambdaFun((int x, int y) -> {
+		// return x + y;
+		// }, 4, 5);
+		// Adder adder = (x, y) -> x + y + x + y;
+		// int sum3 = adder.add(4, 5);
+		// System.out.println(sum + "|" + sum1 + "|" + dif + "|" + sum2 + "|" +
+		// sum3);
+
+		// lambda表达式，方法引用代替lambda表达式
+		// Person[] people = { new Person("zhangsan", 22), new Person("lisi",
+		// 39) };
 		// Comparator<Person> byName = Comparator.comparing(p -> p.getAge());
-		// Arrays.sort(people, byName);				
-//		List<Person> listPerson = new ArrayList<Person>();
-//		listPerson.add(new Person("wangwu", 34));
-//		listPerson.add(new Person("xiaoxiao", 23));
-//		listPerson.sort(Comparator.comparing(p -> p.getAge()));
-//		listPerson.sort(Comparator.comparing(Person::getName));
+		// Arrays.sort(people, byName);
+		// List<Person> listPerson = new ArrayList<Person>();
+		// listPerson.add(new Person("wangwu", 34));
+		// listPerson.add(new Person("xiaoxiao", 23));
+		// listPerson.sort(Comparator.comparing(p -> p.getAge()));
+		// listPerson.sort(Comparator.comparing(Person::getName));
 		System.out.println("hello hello hello");
-
-
+		System.out.println("kitty kitty");
+		System.out.println("!!!!!!");
+		System.out.println("push之后提交 ，amend");
 	}
 
-	
 	static int lambdaFun(Adder value, int x, int y) {
 		return value.add(x, y);
 	}
